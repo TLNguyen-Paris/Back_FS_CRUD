925fc292bf80bbbd7de4a5fc6c2d1d097e30cdcc
Thanh <Thanh.ng.paris@gmail.com>
2024-01-28 02:23:23 +0100

    body-parser middleware

--- server.js
+++ server.js
@@ -4,4 +4,15 @@ require("./config/database");
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(cors({ credentials: true, origin: "*" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.listen("4432");
+
+const UserRoutes = require("./routes/user_routes");
+
+app.use("/fs/api", UserRoutes);
